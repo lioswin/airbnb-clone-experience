bb4ec66dd9a3ee592d27665fbcc4c862ca237bd3
lioswin <lioswinrite@gmail.com>
2022-11-30 13:35:39 +0300

    dummy

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ export default function Hero() {
             <img src="../images/photo-grid.png" className="hero--photo" />
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by 
-            one-of-a-kind hosts—all without leaving home.</p>
+            one-of-a-kind hosts—all without leaving home. <b>Tony Rite wrote the code</b></p>
         </section>
     )
 }
